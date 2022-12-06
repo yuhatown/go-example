487de182b55838dd4ce0c72d97f5ca86cb132235
Taewan Kim <44514409+yuhatown@users.noreply.github.com>
2022-12-06 22:34:39 +0900

    connect mysql & connection error

--- 01/go-chi/go.mod
+++ 01/go-chi/go.mod
@@ -2,4 +2,7 @@ module heesunim/01/go-chi
 
 go 1.19
 
-require github.com/go-chi/chi v1.5.4 // indirect
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+)
--- 01/go-chi/main.go
+++ 01/go-chi/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World Response"))
+	w.Write([]byte("Hello, World"))
 }
 
 func main() {
 	mux := chi.NewRouter()
 	mux.Get("/hello", helloHandler)
-
 	http.ListenAndServe(":8080", mux)
+
+	db, err := sql.Open("mysql", "test:test1234@localhost/test_go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+
+	var answer string
+	db.QueryRow("INSERT INTO test (value) VALUES (1+1)")
+	db.QueryRow("SELECT value FROM test").Scan(&answer)
+	fmt.Println(answer)
 }
+ 
